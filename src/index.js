b12e24212ca58a67606c3708c29741d4dc926d56
mokajima <mokajima.85z@gmail.com>
2018-09-13 13:56:32 +0900

    Code Splitting: Dynamic Imports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import _ from 'lodash';
+function getComponent() {
+  return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
+    let element = document.createElement('div');
 
-function component() {
-  let element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
 
-  return element;
+  }).catch(error => 'An error occurred while loading the component');
 }
 
-document.body.appendChild(component());
+getComponent().then(component => {
+  document.body.appendChild(component);
+})
