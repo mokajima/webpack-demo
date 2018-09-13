2f82b33a7e898f76aa11396ba140e6df8db8c237
mokajima <mokajima.85z@gmail.com>
2018-09-13 10:25:01 +0900

    Code Splitting: Entry Points

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-import {cube} from './math.js';
-
-if (process.env.NODE_ENV !== 'production') {
-  console.log('Looks like we are in development mode!');
-}
+import _ from 'lodash';
 
 function component() {
-  const element = document.createElement('pre');
+  let element = document.createElement('div');
 
-  element.innerHTML = [
-    'Hello webpack!',
-    '5 cubed is equal to ' + cube(5)
-  ].join('\n\n');
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
