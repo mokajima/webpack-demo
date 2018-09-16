0dd7e8a68682477e101f8145f87e940e1dca4d56
mokajima <mokajima.85z@gmail.com>
2018-09-16 17:19:17 +0900

    Caching: Output Filenames

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import _ from 'lodash';
 
 function component() {
-  const element = document.createElement('div');
-  const button = document.createElement('button');
-  const br = document.createElement('br');
+  let element = document.createElement('div');
 
-  button.textContent = 'Click me and look at the console!';
-  element.textContent = _.join(['Hello', 'webpack'], ' ');
-  element.appendChild(br);
-  element.appendChild(button);
-
-  // Note that because a network request is involved, some indication
-  // of loading would need to be shown in a production-level site/app.
-  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
-    const print = module.default;
-
-    print();
-  });
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
